118971008a84cbde184ac66f6b7a3e20f137f4c2
Hansen Wong <hansen.wong@fundnel.com>
2019-01-17 13:28:07 +0700

    init component

---
+++ src/SlackClient.php
@@ -0,0 +1,97 @@
+<?php
+namespace sky\slack;
+
+use yii\httpclient\Client;
+use yii\helpers\Json;
+use Yii;
+use yii\db\BaseActiveRecord;
+use yii\helpers\ArrayHelper;
+
+class SlackClient extends \yii\base\BaseObject
+{
+    public $clientClass = 'yii\httpclient\Client';
+    public $clientOptions = [];
+    public $defaultPayload = [];
+    public $defaultChannel = 'general';
+    public $testerChannel = 'tester';
+    public $offline = false;
+    
+    private $_webhookUrls = [];
+    private $_channel;
+    
+    protected $_client;
+    
+    public function init() {
+        $this->setChannel($this->defaultChannel);
+        return parent::init();
+    }
+    /**
+     * 
+     * @return Client
+     */
+    public function getClient()
+    {
+        if (!$this->_client) {
+            $this->_client = Yii::createObject(array_merge(['class' => $this->clientClass], $this->clientOptions));
+        }
+        return $this->_client;
+    }
+    
+    public function setWebHookUrls(Array $urls)
+    {
+        $this->_webhookUrls = $urls;
+    }
+    
+    public function setChannel($name)
+    {
+        if (!isset($this->_webhookUrls[$name])) {
+            throw new \yii\base\InvalidConfigException($name . ' channel not exsist');
+        }
+        if (YII_ENV == 'dev') {
+            $name = $this->testerChannel;
+        }
+        $this->_channel = $this->_webhookUrls[$name];
+        return $this;
+    }
+    
+    /**
+     * Yii::$app->message->send([
+     *  'text' => 'Yout Text Here'
+     * ]);
+     * 
+     * @param array $payload
+     * @return boolean
+     */
+    
+    public function send($payload)
+    {
+        if ($this->offline) {
+            return true;
+        }
+        $payload = [
+            'payload' => Json::encode(array_merge($this->defaultPayload, $payload)),
+        ];
+        return $this->getClient()->post($this->_channel, $payload)->send();
+    }
+    
+    public function sendText($text, $payload = [])
+    {
+        return $this->send(array_merge($payload, ['text' => $text]));
+    }
+    
+    public static function fieldsAttribute(BaseActiveRecord $model, $attributes = [], $short = true)
+    {
+        $attributes = $attributes ? : $model->attributes();
+        $fields = [];
+        foreach ($attributes as $attribute => $value) {
+            $attribute = is_int($attribute) ? $value : $attribute;
+            $fields[] = [
+                'title' => $model->getAttributeLabel($attribute),
+                'value' => ArrayHelper::getValue($model, $value),
+                'short' => $short,
+            ];
+        }
+        return $fields;
+    }
+
+}
---
+++ src/SlackTarget.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace sky\slack;
+
+use Yii;
+use yii\log\Target;
+
+class SlackTarget extends Target
+{   
+    public $channel = 'general';
+    public $componentName = 'message';
+    
+    public function init() {
+        if (!Yii::$app->{$this->componentName} instanceof \sky\slack\SlackClient) {
+            throw new \yii\base\InvalidConfigException("component {$this->componentName} not set");
+        }
+        return parent::init();
+    }
+
+    public function export()
+    {
+        $text = implode("\n", array_map([$this, 'formatMessage'], $this->messages)) . "\n";
+        $fields = [];
+        if (Yii::$app instanceof \yii\web\Application) {
+            $fields = [
+                [
+                    'title' => 'User',
+                    'value' => !Yii::$app->user->isGuest ? Yii::$app->user->identity->email : '-',
+                    'short' => false,
+                ],
+                [
+                    'title' => 'Url',
+                    'value' => isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '-',
+                    'short' => true,
+                ],
+            ];
+        } elseif (Yii::$app instanceof \yii\web\Application) {
+            $fields = [
+                [
+                    'title' => 'Type',
+                    'value' => 'console',
+                    'short' => true,
+                ],
+            ];
+        }
+        $fields[] = [
+            'title' => 'Controller Action',
+            'value' => Yii::$app->controller ? Yii::$app->controller->id . '/' . Yii::$app->controller->action->id : null,
+            'short' => true
+        ];
+        $fields[] = [
+            'title' => 'Level',
+            'value' => $this->levels,
+            'short' => true
+        ];
+        $fields[] = [
+            'title' => 'Debug Model',
+            'value' => YII_DEBUG,
+            'short' => true
+        ];
+        Yii::$app->{$this->componentName}->setChannel($this->channel)->send([
+            'text' => "ERROR LEVEL {$this->levels} - " . Yii::$app->name,
+            'attachments' => [
+                [
+                    'color' => "#ff0000",
+                    'text' => $text,
+                    'fields' => $fields,
+                ]
+            ]
+        ]);
+    }
+}
