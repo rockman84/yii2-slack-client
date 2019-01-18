2876ef3d1246c6dc7a181a45a65b42e19ba2c0d8
Hansen Wong <hansen.wong@fundnel.com>
2019-01-18 16:19:48 +0700

    update fields error target

--- src/SlackTarget.php
+++ src/SlackTarget.php
@@ -5,6 +5,9 @@ namespace sky\slack;
 use Yii;
 use yii\log\Target;
 
+/**
+ * @property array $fields
+ */
 class SlackTarget extends Target
 {   
     /**
@@ -31,6 +34,21 @@ class SlackTarget extends Target
     public function export()
     {
         $text = implode("\n", array_map([$this, 'formatMessage'], $this->messages)) . "\n";
+        
+        Yii::$app->{$this->componentName}->setChannel($this->channel)->send([
+            'text' => "ERROR LEVEL {$this->levels} - " . Yii::$app->name,
+            'attachments' => [
+                [
+                    'color' => "#ff0000",
+                    'text' => $text,
+                    'fields' => $this->fields,
+                ]
+            ]
+        ]);
+    }
+    
+    public function getFields()
+    {
         $fields = [];
         if (Yii::$app instanceof \yii\web\Application) {
             $fields = [
@@ -84,15 +102,5 @@ class SlackTarget extends Target
             'value' => YII_DEBUG,
             'short' => true
         ];
-        Yii::$app->{$this->componentName}->setChannel($this->channel)->send([
-            'text' => "ERROR LEVEL {$this->levels} - " . Yii::$app->name,
-            'attachments' => [
-                [
-                    'color' => "#ff0000",
-                    'text' => $text,
-                    'fields' => $fields,
-                ]
-            ]
-        ]);
     }
 }
